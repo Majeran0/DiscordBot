ed6f30977a50ca71408c7232c38ea2e1a8b06f2c
Majeran0 <tropok@gmail.com>
2019-04-24 21:48:00 +0200

    Add project files.

---
+++ DiscordBot/Modules/DJTS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules {
+    [Name("DJTS")]
+    public class DJTS : ModuleBase<SocketCommandContext> {
+        [Command("2137")]
+        [Summary("No 2137.")]
+        public async Task DJTSAsync() {
+            var current = DateTime.Now;
+            var dj = new DateTime(current.Year, current.Month, current.Day, 21, 37, 0);
+            var djj = new DateTime(current.Year, current.Month, current.Day + 1, 21, 37, 0);
+            if (current.Hour == 21 && current.Minute == 37) {
+                await ReplyAsync("Papaj umar");
+            } else {
+                var interval = current < dj ? dj - current : djj - current;
+                await ReplyAsync("Do 21:37 zostało " + interval.Hours + ":" + interval.Minutes + ":" + interval.Seconds);
+            }
+        }
+    }
+}
+
+
+
+
---
+++ DiscordBot/Services/StartupService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Services {
+    public class StartupService {
+        private readonly IServiceProvider _provider;
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+
+        public StartupService(
+            IServiceProvider provider,
+            DiscordSocketClient discord,
+            CommandService commands,
+            IConfigurationRoot config) {
+            _provider = provider;
+            _config = config;
+            _commands = commands;
+            _discord = discord;
+        }
+
+        public async Task StartAsync() {
+            string discordToken = _config.GetSection("token").Value;
+            if (string.IsNullOrWhiteSpace(discordToken))
+                throw new Exception("You must enter your discord token to _config.yml");
+
+            await _discord.LoginAsync(TokenType.Bot, discordToken);
+            await _discord.StartAsync();
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+    }
+}
